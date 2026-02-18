5c1a0ca00313a6ded9630a44c4a137bae2824a94
Ryuma <ryumabissembay@gmail.com>
2026-02-18 13:27:29 +0500

    fix the DB to Atlas

--- config/database.js
+++ config/database.js
@@ -1,18 +1,9 @@
 const mongoose = require('mongoose');
 
-const { MongoMemoryServer } = require('mongodb-memory-server');
-
-let mongod;
-
-
-
 const connectDB = async () => {
   try {
-    mongod = await MongoMemoryServer.create();
-    const uri = mongod.getUri();
-
-    await mongoose.connect(uri);
-    console.log('MongoDB Memory Server connected!');
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB Atlas connected!');
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
